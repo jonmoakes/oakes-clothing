5c50ff3287757c7d26e82b9f9519d7b3f7ca3b12
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2019-07-18 10:42:31 +0100

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.coponent';
+
+function App() {
+  return <div>
+    <HomePage />
+  </div>;
+}
+
+export default App;
