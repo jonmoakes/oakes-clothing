d78484d26c93f00ccafaaee6a07101d85ca71bef
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2019-08-13 18:13:33 +0100

    added sagas to sign ins

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -7,15 +7,13 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
-        case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
+        case UserActionTypes.SIGN_IN_SUCCESS:
             return {
                 ...state,
                 currentUser: action.payload,
                 error: null
             };
-        case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
-        case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+        case UserActionTypes.SIGN_IN_FAILURE:
             return {
                 ...state,
                 error: action.payload
@@ -27,3 +25,28 @@ const userReducer = (state = INITIAL_STATE, action) => {
 
 export default userReducer;
 
+// import UserActionTypes from './user.types';
+ 
+// const INITIAL_STATE = {
+//   currentUser: null,
+// };
+ 
+// const userReducer = (state = INITIAL_STATE, action) => {
+//   switch (action.type) {
+//     case UserActionTypes.SIGN_IN_SUCCESS:
+//       return {
+//         ...state,
+//         currentUser: action.payload,
+//         error: null,
+//       };
+//     case UserActionTypes.SIGN_IN_FAILURE:
+//       return {
+//         ...state,
+//         error: action.payload
+//       }
+//     default:
+//       return state;
+//   }
+// };
+ 
+// export default userReducer;
