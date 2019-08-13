f831cf9b129ed6254f1254c2dfbe4ba26e549282
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2019-08-13 19:12:52 +0100

    added saga to signout

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -5,6 +5,8 @@ import UserActionTypes from './user.types';
 import { 
     signInSuccess, 
     signInFailure,
+    signOutSuccess,
+    signOutFailure
 } from './user.actions';
 
 import { 
@@ -52,22 +54,36 @@ export function* isUserAuthenticated() {
     }
 }
 
+export function* signOut() {
+    try {
+        yield auth.signOut();
+        yield put(signOutSuccess());
+    } catch (error) {
+        yield put(signOutFailure(error));
+    }
+}
+
 export function* onGoogleSignInStart() {
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
 export function* onEmailSignInStart() {
-    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail )
+    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail );
 }
 
 export function* onCheckUserSession() {
-    yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated)
+    yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+export function*  onSignOutStart() {
+    yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
 }
 
 export function* userSagas() {
     yield all([
         call(onGoogleSignInStart), 
         call(onEmailSignInStart), 
-        call(isUserAuthenticated)
+        call(isUserAuthenticated),
+        call(onSignOutStart)
     ]);
 }
