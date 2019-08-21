e3065fc6360b15dabba580241f9b9775ddfcb980
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2019-08-21 14:49:24 +0100

    corrected typo on compression

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('comopression');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
