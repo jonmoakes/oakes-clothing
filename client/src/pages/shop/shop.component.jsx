246c30956932aa8dca9bc93ea816d6733c03f5b5
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2019-08-21 11:58:36 +0100

    Added error bounday

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -29,7 +29,7 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
             component={CollectionPageContainer}
           />
           </Suspense>
-          </ShopPageContainer>
+        </ShopPageContainer>
     ); 
   }
 
